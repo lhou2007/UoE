31f580f9eddaf0ce2a4c20efe80c5f1225512484
lhou2007 <lmhou2007@outlook.com>
2025-10-20 15:29:23 +0100

    smartsort compiles

--- smartsort.py
+++ smartsort.py
@@ -25,15 +25,15 @@ sortedRunThreshold = 10
 # In-place Insert Sort on A[m],...,A[n-1]:
 
 def insertSort(A,m,n):
-"""In-place Insert Sort on A[m],..,A[n-1]"""
+#"""In-place Insert Sort on A[m],..,A[n-1]"""
 
-# TASK1.1 (5 marks)
-    for i in range(1, len(A, m, n))#for i = 1 to |A| - 1 #|A| = size of A
-     x = A[i]#x = key
-     j = i-1
-     while j>=0 and A[j] > x
-           A[j+1] = A[j]
-           j =- 1 #j = j-1
+# TASK1.1 (5 marks)    
+    for i in range(1, len(A, m, n)):#for i = 1 to |A| - 1 #|A| = size of A
+        x = A[i]
+        j = i-1
+        while j>=0 and A[j] > x:
+            A[j+1] = A[j]
+            j -= 1 #j = j-1
         A[j+1] = x
 # end
 
